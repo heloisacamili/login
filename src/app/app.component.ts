ab5f062874018deb4a5789b4f34fe3a3b0699999
heloisacamili <heloisa.camilii@gmail.com>
2022-09-16 13:27:11 -0300

    adicionando o login

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,10 @@
 import { Component } from '@angular/core';
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
 
 @Component({
   selector: 'app-root',
@@ -7,4 +13,21 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'login';
+  cpf : string = '';
+  password : string = '';
+
+  constructor(private httpClient : HttpClient){
+
+  }
+
+  public login(){
+    this.httpClient.get('http://localhost:3019/clientes',).toPromise().then((response : any) => {
+      console.log(response);
+    })}
+
+    public logar(){
+      this.httpClient.post('http://localhost:3019/login', {cpf : this.cpf, password : this.password}).toPromise().then((response : any) => {
+        console.log(response);
+      })
+    }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { FormsModule} from '@angular/forms';
+
+@Injectable()
+export class ConfigService {
+  constructor(private http: HttpClient) { }
+}
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpClientModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [
+    AppComponent,
+  ],
+  bootstrap: [ AppComponent ]
 })
-export class AppModule { }
+export class AppModule {}
